0f4ec432516644f7d8ab13e2e56e7527889e6257
Ivan_Poliakov <pol13112005@gmail.com>
2024-01-25 09:46:59 +0300

    const ints

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -1,5 +1,10 @@
 #include "is_leap.h"
 
 bool IsLeap(int year) {
-    return true;
+    bool ans = true;
+    const int a = 4;
+    const int b = 100;
+    const int c = 400;
+    ans = (year % a == 0) && (year % b != 0) && (year % c == 0);
+    return ans;
 }
