ba1de0d160f7692854f896f84c6c6528857171ae
Ivan_Poliakov <pol13112005@gmail.com>
2024-01-25 16:36:28 +0300

    fix 1 and 0

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,12 +1,15 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
-    int64_t first_fib = 0;
+    int64_t first_fib = 1;
     int64_t second_fib = 1;
-    if (n == 1) {
-        return first_fib;
+    if (n == 0) {
+        return 0;
     }
-    for (int i = 2; i < n; ++i) {
+    if (n == 1) {
+        return 1;
+    }
+    for (int64_t i = 2; i < n; ++i) {
         int64_t cur_fib = first_fib + second_fib; // считаю очередное число фибоначи
         first_fib = second_fib;
         second_fib = cur_fib;
