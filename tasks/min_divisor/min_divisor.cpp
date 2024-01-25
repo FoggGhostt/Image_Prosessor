d6455f05e4862e68b953b97ab5adbc70fb4a2d76
Ivan_Poliakov <pol13112005@gmail.com>
2024-01-26 01:12:16 +0300

    simple sol

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,5 +1,13 @@
 #include "min_divisor.h"
 
 int64_t MinDivisor(int64_t number) {
-    return 2;
+    if ((number == 1) || (number == 2)) {
+        return number;
+    }
+    for (int64_t i = 2; i * i < number + 1; ++i) {
+        if (number % i == 0) {
+            return i;
+        }
+    }
+    return number;
 }
