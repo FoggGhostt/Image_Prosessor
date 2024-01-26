7a137b06d943a3d5dbd7579adf73864c62d4d7e6
Ivan_Poliakov <pol13112005@gmail.com>
2024-01-26 11:43:50 +0300

    long cicle

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -2,5 +2,17 @@
 
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
-    return 1;
+    int64_t num = numerator;
+    int64_t den = denominator;
+    for (;;) {
+        for (int64_t i = 2; i * i < numerator + 1; ++numerator) {
+            if ((num % i == 0) && (den % i == 0)) {
+                num = num / i;
+                den = den / i;
+                break;
+            }
+            return (num + den);
+        }
+        return num + den;
+    }
 }
