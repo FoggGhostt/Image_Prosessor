3dd2491a7f70b316f707c5b6e901517d76e49238
Ivan_Poliakov <pol13112005@gmail.com>
2024-01-30 16:32:15 +0300

    f

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -4,6 +4,6 @@
 bool IsLeap(int year) {
     const int first_cond = 4;
     const int second_cond = 100;
-    const int third_cond = 400;  // some case constants
+    const int third_cond = 400; 
     return (((year % first_cond == 0) && (year % second_cond != 0)) || (year % third_cond == 0));
 }
