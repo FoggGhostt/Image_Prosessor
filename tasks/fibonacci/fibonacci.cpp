4d01984bbc447657c009cefac3bca00d6411e0db
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 00:32:24 +0300

    fix sicle

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -1,18 +1,16 @@
 #include "fibonacci.h"
 
 int64_t CalculateNthFibonacci(int64_t n) {
+    int64_t zero_fib = 0;
     int64_t first_fib = 1;
-    int64_t second_fib = 1;
-    if (n == 0) {
-        return 0;
+    switch (n) {
+        case 0: return 0;
+        case 1: return 1;
     }
-    if (n == 1) {
-        return 1;
+    for (int64_t i = 2; i <= n; ++i) {
+        int64_t intermediate_fib = first_fib;
+        first_fib = first_fib + zero_fib;
+        zero_fib = intermediate_fib;
     }
-    for (int64_t i = 2; i < n; ++i) {
-        int64_t cur_fib = first_fib + second_fib; // считаю очередное число фибоначи
-        first_fib = second_fib;
-        second_fib = cur_fib;
-    }
-    return second_fib;
+    return first_fib;
 }
