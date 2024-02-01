e7ac8aaa2a0ee02eb56cb6b87e584a80b5d3b3dc
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 00:15:12 +0300

    divide lines

--- tasks/is_leap/is_leap.cpp
+++ tasks/is_leap/is_leap.cpp
@@ -7,5 +7,6 @@ const int FALSE_CYCLE = 400;
 }  // namespace is_leap
 
 bool IsLeap(int year) {
-    return ((year % is_leap::TRUE_SMALL_CYCLE == 0) && (year % is_leap::TRUE_LARGE_CYCLE != 0)) || (year % is_leap::FALSE_CYCLE == 0);
+    return ((year % is_leap::TRUE_SMALL_CYCLE == 0) && (year % is_leap::TRUE_LARGE_CYCLE != 0)) || 
+           (year % is_leap::FALSE_CYCLE == 0);
 }
