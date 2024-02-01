b8b94ab1c1bbe8d0fb769d016bc1a6c9ea8e5944
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-01 23:56:34 +0300

    fix extra cases

--- tasks/min_divisor/min_divisor.cpp
+++ tasks/min_divisor/min_divisor.cpp
@@ -1,10 +1,7 @@
 #include "min_divisor.h"
 
 int64_t MinDivisor(int64_t number) {
-    if ((number == 1) || (number == 2)) {
-        return number;
-    }
-    for (int64_t i = 2; i * i < number + 1; ++i) {
+    for (int64_t i = 2; i * i <= number; ++i) {
         if (number % i == 0) {
             return i;
         }
