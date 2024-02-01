977beb69f0f0044ae62d5678adda74844ff3d533
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 00:37:27 +0300

    fix sicl1e

--- tasks/fibonacci/fibonacci.cpp
+++ tasks/fibonacci/fibonacci.cpp
@@ -3,11 +3,8 @@
 int64_t CalculateNthFibonacci(int64_t n) {
     int64_t zero_fib = 0;
     int64_t first_fib = 1;
-    switch (n) {
-        case 0: 
-            return 0;
-        case 1: 
-            return 1;
+    if (n == 0) {
+        return zero_fib;
     }
     for (int64_t i = 2; i <= n; ++i) {
         int64_t intermediate_fib = first_fib;
