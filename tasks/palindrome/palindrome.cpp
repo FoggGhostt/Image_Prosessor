6f535aaf24110492ee66ce1305aaaea5244b4127
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 14:02:22 +0300

    fix order of conditions

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,13 +1,13 @@
 #include "palindrome.h"
 
-int64_t MoveLeftPointer(const std::string* str, int64_t i) {
+size_t MoveLeftPointer(const std::string* str, size_t i) {
     do {
         ++i;
     } while ((*str)[i] == ' ');
     return i;
 }
 
-int64_t Moverightpointer(const std::string* str, int64_t j) {
+size_t Moverightpointer(const std::string* str, size_t j) {
     do {
         --j;
     } while ((*str)[j] == ' ');
@@ -15,16 +15,16 @@ int64_t Moverightpointer(const std::string* str, int64_t j) {
 }
 
 bool IsPalindrome(const std::string& str) {
-    int64_t length = std::size(str);
-    int64_t i = 0;           // left pointer
-    int64_t j = length - 1;  // right pointer
+    size_t length = std::size(str);
+    size_t i = 0;           // left pointer
+    size_t j = length - 1;  // right pointer
     const std::string* ptr = &str;
     while (i <= j) {
-        i = MoveLeftPointer(ptr, i);
-        j = Moverightpointer(ptr, j);
-        if (i >= length || j <= -1 || str[i] != str[j]) {
+        if (str[i] != str[j]) {
             return false;
         }
+        i = MoveLeftPointer(ptr, i);
+        j = Moverightpointer(ptr, j);
     }
     return true;
 }
