9741a1b85304a3c37745f2131d1918f4ba345611
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 13:13:15 +0300

    1gcd

--- tasks/reduce_fraction/reduce_fraction.cpp
+++ tasks/reduce_fraction/reduce_fraction.cpp
@@ -1,18 +1,15 @@
 #include "reduce_fraction.h"
-
+#include <algorithm>
 
 int64_t ReduceFraction(int64_t numerator, int64_t denominator) {
     int64_t num = numerator;
     int64_t den = denominator;
-    for (;;) {
-        for (int64_t i = 2; i * i < numerator + 1; ++numerator) {
-            if ((num % i == 0) && (den % i == 0)) {
-                num = num / i;
-                den = den / i;
-                break;
-            }
-            return (num + den);
+    while (std::min(num, den) > 0) {  // gcd algorithm
+        if (num > den) {
+            num = num - den;
+        } else {
+            den = den - num;
         }
-        return num + den;
     }
+    return numerator / std::max(num, den) + denominator / std::max(num, den);
 }
