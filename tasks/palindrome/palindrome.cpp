eecb312eac178d7001779e739547279ae45c92ad
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 13:55:02 +0300

    fix sicle in movepoint

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,22 +1,22 @@
 #include "palindrome.h"
 
 int64_t MoveLeftPointer(const std::string* str, int64_t i) {
-    while ((*str)[i] == ' ') {
+    do {
         ++i;
-    }
+    } while ((*str)[i] == ' ');
     return i;
 }
 
 int64_t Moverightpointer(const std::string* str, int64_t j) {
-    while ((*str)[j] == ' ') {
+    do {
         --j;
-    }
+    } while ((*str)[j] == ' ');
     return j;
 }
 
 bool IsPalindrome(const std::string& str) {
     int64_t length = std::size(str);
-    int64_t i = 0;  // left pointer
+    int64_t i = 0;           // left pointer
     int64_t j = length - 1;  // right pointer
     const std::string* ptr = &str;
     while (i <= j) {
