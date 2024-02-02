01631033c57ba81c4bd2ea89c9996df3442a11d8
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-02 14:12:30 +0300

    some fix

--- tasks/palindrome/palindrome.cpp
+++ tasks/palindrome/palindrome.cpp
@@ -1,30 +1,33 @@
 #include "palindrome.h"
 
 size_t MoveLeftPointer(const std::string* str, size_t i) {
-    do {
+    while (i < str->size() && (*str)[i] == ' ') {
         ++i;
-    } while ((*str)[i] == ' ');
+    }
     return i;
 }
 
 size_t Moverightpointer(const std::string* str, size_t j) {
-    do {
+    while (j > 0 && (*str)[j] == ' ') {
         --j;
-    } while ((*str)[j] == ' ');
+    }
     return j;
 }
 
 bool IsPalindrome(const std::string& str) {
-    size_t length = std::size(str);
+    size_t length = str.size();
+    if (length < 2) {
+        return true;
+    }
     size_t i = 0;           // left pointer
     size_t j = length - 1;  // right pointer
     const std::string* ptr = &str;
-    while (i <= j) {
+    while (i < j) {
+        i = MoveLeftPointer(ptr, i);
+        j = Moverightpointer(ptr, j);
         if (str[i] != str[j]) {
             return false;
         }
-        i = MoveLeftPointer(ptr, i);
-        j = Moverightpointer(ptr, j);
     }
     return true;
 }
