7dc39c88149263a230531b70b5e5ef47bd2e67d8
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-03 15:16:54 +0300

    no for push

--- tasks/password/password.cpp
+++ tasks/password/password.cpp
@@ -1,5 +1,26 @@
 #include "password.h"
 
-bool ValidatePassword(const std::string& password) {
-    return false;
+bool IsCorrectASCI(const std::string *password) {
+    const int left_border = 33;
+    const int right_border = 126;
+    for (size_t i = 0; i < (*password).size(); ++i) {
+        if ((*password)[i] < left_border || (*password)[i] > right_border) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool CheckLength(const std::string *password) {
+    const int min_length = 8;
+    const int max_length = 14;
+    return (*password).size() >= min_length && (*password).size() <= max_length;
+}
+
+bool Check3Conditions(const std::string *password) {
+    
+}
+
+bool ValidatePassword(const std::string &password) {
+    const std::string *ptr = &password;
 }
