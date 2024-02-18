29194be7eed43068c94d99defecf284a4aaf8f59
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-18 13:28:06 +0300

    deque solution

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -6,24 +6,17 @@
 #include <cstddef>
 #include <iterator>
 #include <string>
+#include <deque>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::vector<std::string> top_side_students = {};
-    std::vector<std::string> bottom_side_students = {};
-    for (size_t i = 0; i < student_actions.size(); ++i) {
-        if (student_actions[i].side == Side::Bottom) {
-            bottom_side_students.push_back(student_actions[i].name);
-        } else if (student_actions[i].side == Side::Top) {
-            top_side_students.push_back(student_actions[i].name);
+    std::deque<std::string> all_students; 
+    for (const StudentAction& student : student_actions) {
+        if (student.side == Side::Bottom) {
+            all_students.push_back(student.name);
+        } else if (student.side == Side::Top){
+            all_students.push_front(student.name);
         }
-    }
-    std::vector<std::string> all_students = {};
-    for (size_t j = top_side_students.size() - 1; ~j; --j) {
-        all_students.push_back(top_side_students[j]);
-    }
-    for (size_t k = 0; k < bottom_side_students.size(); ++k) {
-        all_students.push_back(bottom_side_students[k]);
     }
     std::vector<std::string> queue_of_all_students = {};
     for (size_t num : queries) {
