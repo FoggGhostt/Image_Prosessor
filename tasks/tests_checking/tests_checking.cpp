d13abf61a64c38d87c01f7ca0838887da52bd3ba
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-18 17:42:30 +0300

    add string_view

--- tasks/tests_checking/tests_checking.cpp
+++ tasks/tests_checking/tests_checking.cpp
@@ -1,26 +1,23 @@
 #include "tests_checking.h"
 
 #include <iostream>
-#include <array>
-#include <algorithm>
-#include <cstddef>
-#include <iterator>
-#include <string>
 #include <deque>
+#include <string_view>
 
 std::vector<std::string> StudentsOrder(const std::vector<StudentAction>& student_actions,
                                        const std::vector<size_t>& queries) {
-    std::deque<std::string> all_students;
+    std::deque<std::string_view> all_students;
     for (const StudentAction& student : student_actions) {
         if (student.side == Side::Bottom) {
-            all_students.push_back(student.name);
+            all_students.emplace_back(student.name);
         } else if (student.side == Side::Top) {
-            all_students.push_front(student.name);
+            all_students.emplace_front(student.name);
         }
     }
     std::vector<std::string> queue_of_all_students = {};
     for (size_t num : queries) {
-        queue_of_all_students.push_back(all_students[num - 1]);
+        std::string new_string(all_students[num - 1]);
+        queue_of_all_students.push_back(new_string);
     }
     return queue_of_all_students;
 }
