11aa2eefe32851f34b2ed5d3417fa5f560d07aab
Ivan_Poliakov <pol13112005@gmail.com>
2024-02-19 19:49:27 +0300

    i am tired))

--- tasks/scorer/scorer.cpp
+++ tasks/scorer/scorer.cpp
@@ -1,80 +1,83 @@
 #include "scorer.h"
+
 #include <algorithm>
-#include <climits>
-#include <string>
-#include <utility>
 #include <iostream>
+#include <set>
+#include <string>
+#include <vector>
 
-bool EventsComparatop(const Event& event_1, const Event& event_2) {
-    const std::string name1 = event_1.student_name;
-    const std::string name2 = event_2.student_name;
-    const std::string task_name1 = event_1.task_name;
-    const std::string task_name2 = event_2.task_name;
-    const int64_t time1 = event_1.time;
-    const int64_t time2 = event_2.time;
-    return std::tie(name1, task_name1, time1) < std::tie(name2, task_name2, time2);
-}
-
-struct CurentTask {
+struct TaskCondition {
     std::string cur_student;
     std::string cur_task_name = "nothing";
     bool is_merge_request_open;
     bool is_checked;
-    CurentTask() : cur_student("nothing"), cur_task_name("nothing"), is_merge_request_open(false), is_checked(false) {
+    TaskCondition()
+        : cur_student("nothing"), cur_task_name("nothing"), is_merge_request_open(false), is_checked(false) {
     }
 };
 
-void AddTask(ScoreTable& table, const CurentTask& cur_task) {
-    if (cur_task.cur_student != "nothing" && cur_task.is_checked == true && cur_task.is_merge_request_open == false) {
-        auto iter = table.find(cur_task.cur_student);
-        if (iter != table.end()) {
-            iter->second.insert(cur_task.cur_task_name);
-        } else {
-            std::set<TaskName> task_set = {};
-            task_set.insert(cur_task.cur_task_name);
-            std::pair<std::string, std::set<TaskName>> pair(cur_task.cur_student, task_set);
-            table.insert(pair);
-        }
-    }
+using StudentTable = std::map<StudentName, std::map<TaskName, TaskCondition>>;
+
+bool EventsTimeComparatop(const Event& event_1, const Event& event_2) {
+    return event_1.time < event_2.time;
 }
 
-void SetCheckMergeStatus(CurentTask& cur_task, Event& cur_event, time_t time) {
-    if (cur_event.time <= time) {
-        switch (cur_event.event_type) {
-            case EventType::CheckFailed:
-                cur_task.is_checked = false;
-                break;
-            case EventType::CheckSuccess:
-                cur_task.is_checked = true;
-                break;
-            case EventType::MergeRequestOpen:
-                cur_task.is_merge_request_open = true;
-                break;
-            case EventType::MergeRequestClosed:
-                cur_task.is_merge_request_open = false;
-                break;
-        }
+void SetTaskCondition(const Event& event, StudentTable& stud_table) {
+    auto iter_stud = stud_table.find(event.student_name);
+    if (iter_stud == stud_table.end()) {
+        std::string name = event.student_name;
+        std::map<TaskName, TaskCondition> tasks = {};
+        std::pair<std::string, std::map<TaskName, TaskCondition>> new_stud(name, tasks);
+        stud_table.insert(new_stud);
     }
+    auto iter_task = stud_table[event.student_name].find(event.task_name);
+    if (iter_task == stud_table[event.student_name].end()) {
+        TaskName task = event.task_name;
+        TaskCondition task_cond = {};
+        std::pair<TaskName, TaskCondition> new_task(task, task_cond);
+    }
+    switch (event.event_type) {
+        case EventType::CheckFailed:
+            stud_table[event.student_name][event.task_name].is_checked = false;
+            break;
+        case EventType::CheckSuccess:
+            stud_table[event.student_name][event.task_name].is_checked = true;
+            break;
+        break;
+        case EventType::MergeRequestOpen:
+            stud_table[event.student_name][event.task_name].is_merge_request_open = true;
+            break;
+        case EventType::MergeRequestClosed:
+            stud_table[event.student_name][event.task_name].is_merge_request_open = false;
+            break;
+        }
 }
 
 ScoreTable GetScoredStudents(const Events& events, time_t score_time) {
     ScoreTable answer_table = {};
-    std::vector<Event> sortedbly_events = {};
+    StudentTable student_table = {};
+    std::vector<Event> sortedbly_student = {};
     for (const Event& event : events) {
-        sortedbly_events.push_back(event);
+         sortedbly_student.emplace_back(event);
     }
-    std::sort(sortedbly_events.begin(), sortedbly_events.end(), EventsComparatop);
-    Event pseudo = {};
-    pseudo.student_name = "new event for update last task of last student ";
-    sortedbly_events.push_back(pseudo);
-    CurentTask cur_task = {};
-    for (Event& event : sortedbly_events) {
-        if (event.student_name != cur_task.cur_student || event.task_name != cur_task.cur_task_name) {
-            AddTask(answer_table, cur_task);
-            cur_task.cur_student = event.student_name;
-            cur_task.cur_task_name = event.task_name;
+    std::sort(sortedbly_student.begin(), sortedbly_student.end(), EventsTimeComparatop);
+    for (const Event& event : sortedbly_student) {
+        if (event.time <= score_time) {
+            SetTaskCondition(event, student_table);
         }
-        SetCheckMergeStatus(cur_task, event, score_time);
+    }
+    for (auto& name : student_table) {
+        for (auto& task : name.second) {
+            if (task.second.is_checked == true && task.second.is_merge_request_open == false) {
+                auto iter_stud_set = answer_table.find(name.first);
+                if (iter_stud_set == answer_table.end()) {
+                    auto empty_set_of_tasks = std::set<TaskName>();
+                    std::pair<StudentName, std::set<TaskName>> new_stud(name.first, empty_set_of_tasks);
+                    answer_table.insert(new_stud);
+                }
+                answer_table[name.first].insert(task.first);
+            }
+        }
     }
     return answer_table;
 }
