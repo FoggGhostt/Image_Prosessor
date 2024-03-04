b31edb37ff402ef98e1cc5c7543f7ccabbbc8606
Ivan_Poliakov <pol13112005@gmail.com>
2024-03-04 21:17:42 +0300

    fix true sol

--- tasks/stack/stack.cpp
+++ tasks/stack/stack.cpp
@@ -14,10 +14,41 @@ Stack::~Stack() {
     }
 }
 
-Stack::Push(int32_t value) {
+void Stack::Push(int32_t value) {
     Node* new_top = new Node{value, head_};
     head_ = new_top;
     size_ += 1;
 }
 
+void Stack::Clear() {
+     while (!Empty()) {
+            Pop();
+    }
+}
+
+void Stack::Pop() {
+    if (Empty()) {
+        return;
+    }
+    Node* old_top = head_->previous;
+    delete head_;
+    head_ = old_top;
+    size_ -= 1;
+}
+
+int32_t Stack::Top() const {
+    if (Empty()) {
+        return {};
+    }
+    int32_t value = head_->value;
+    return value;
+}
+
+int32_t Stack::Size() const {
+    return size_;
+}
+
+bool Stack::Empty() const {
+    return head_ == nullptr;
+}
 
