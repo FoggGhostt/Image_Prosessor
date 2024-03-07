872dad0e5ffe18d563d9154e4e6c09767a01a10d
Ivan_Poliakov <pol13112005@gmail.com>
2024-03-07 13:02:30 +0300

    priority queue sol

--- tasks/provinces/provinces.cpp
+++ tasks/provinces/provinces.cpp
@@ -2,78 +2,23 @@
 
 #include <algorithm>
 #include <deque>
+#include <functional>
 #include <iostream>
+#include <queue>
 
 int64_t CountPassports(const std::vector<int>& provinces) {
-    if (provinces.size() == 1) {
-        return provinces[0];
+    int64_t answer = 0;
+    std::priority_queue<int64_t, std::vector<int64_t>, std::greater<int64_t>> province_que = {};
+    for (int kindom : provinces) {
+        province_que.push(kindom);
     }
-    if (provinces.size() == 2) {
-        return provinces[0] + provinces[1];
-    }
-    std::vector<int> sortable_provinces = {};
-    for (int i : provinces) {
-        sortable_provinces.push_back(i);
-    }
-    std::sort(sortable_provinces.begin(), sortable_provinces.end());
-    std::vector<int64_t> combined_province = {};
-    combined_province.push_back(sortable_provinces[0] + sortable_provinces[1]);
-    int64_t answer = sortable_provinces[0] + sortable_provinces[1];
-    size_t i = 2;
-    size_t j = 0;
-    while (i < sortable_provinces.size() || j < combined_province.size() - 1) {
-        if (i == sortable_provinces.size() - 1 && j == combined_province.size() - 1) {
-            answer += sortable_provinces[i] + combined_province[j];
-            return answer;
-        } else if (i == sortable_provinces.size()) {
-            answer += combined_province[j] + combined_province[j + 1];
-            combined_province.push_back(combined_province[j] + combined_province[j + 1]);
-            j += 2;
-        } else if (j == combined_province.size()) {
-            answer += sortable_provinces[i] + sortable_provinces[i + 1];
-            combined_province.push_back(sortable_provinces[i] + sortable_provinces[i + 1]);
-            i += 2;
-        } else if (i == sortable_provinces.size() - 1 && j != combined_province.size() - 1) {
-            if (sortable_provinces[i] + combined_province[j] < combined_province[j] + combined_province[j + 1]) {
-                answer += sortable_provinces[i] + combined_province[j];
-                combined_province.push_back(sortable_provinces[i] + combined_province[j]);
-                i += 1;
-                j += 1;
-            } else {
-                j += 2;
-                answer += combined_province[j] + combined_province[j + 1];
-                combined_province.push_back(combined_province[j] + combined_province[j + 1]);
-            }
-        } else if (i != sortable_provinces.size() - 1 && j == combined_province.size() - 1) {
-            if (sortable_provinces[i] + combined_province[j] < sortable_provinces[i] + sortable_provinces[i + 1]) {
-                answer += sortable_provinces[i] + combined_province[j];
-                combined_province.push_back(sortable_provinces[i] + combined_province[j]);
-                i += 1;
-                j += 1;
-            } else {
-                answer += sortable_provinces[i] + sortable_provinces[i + 1];
-                combined_province.push_back(sortable_provinces[i] + sortable_provinces[i + 1]);
-                i += 2;
-            }
-        } else {
-            if (sortable_provinces[i] + combined_province[j] < combined_province[j] + combined_province[j + 1] &&
-                sortable_provinces[i] + combined_province[j] < sortable_provinces[i] + sortable_provinces[i + 1]) {
-                answer += sortable_provinces[i] + combined_province[j];
-                combined_province.push_back(sortable_provinces[i] + combined_province[j]);
-                i += 1;
-                j += 1;
-            } else if (combined_province[j] + combined_province[j + 1] < sortable_provinces[i] + combined_province[j] &&
-                       combined_province[j] + combined_province[j + 1] <
-                           sortable_provinces[i] + sortable_provinces[i + 1]) {
-                answer += combined_province[j] + combined_province[j + 1];
-                combined_province.push_back(combined_province[j] + combined_province[j + 1]);
-                j += 2;
-            } else {
-                answer += sortable_provinces[i] + sortable_provinces[i + 1];
-                combined_province.push_back(sortable_provinces[i] + sortable_provinces[i + 1]);
-                i += 2;
-            }
-        }
+    while (province_que.size() > 1) {
+        int64_t first = province_que.top();
+        province_que.pop();
+        int64_t second = province_que.top();
+        province_que.pop();
+        answer += first + second;
+        province_que.push(first + second);
     }
     return answer;
 }
